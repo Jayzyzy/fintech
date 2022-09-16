56b1af28856da2006ecdc81bfe0d5caa8ebaeb12
Jayzyzy <Jayzyzy@outlook.com>
2022-09-16 10:27:19 +0800

    s

---
+++ sql/MOCK_DATA.sql
@@ -0,0 +1,18 @@
+create table MOCK_DATA (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (1, 'Holden', 'Jagels', 'hjagels0@symantec.com', 'Male', '23.65.27.127');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (2, 'Onfroi', 'Iley', 'oiley1@alexa.com', 'Male', '78.17.11.53');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (3, 'Alfie', 'Draaisma', 'adraaisma2@jiathis.com', 'Female', '151.83.53.207');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (4, 'Rosella', 'McGraw', 'rmcgraw3@weather.com', 'Female', '56.134.160.177');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (5, 'Reginauld', 'Owttrim', 'rowttrim4@goodreads.com', 'Male', '32.74.203.95');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (6, 'Dill', 'Chamley', 'dchamley5@cam.ac.uk', 'Male', '40.159.121.97');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (7, 'Vladimir', 'Wanley', 'vwanley6@yellowbook.com', 'Male', '116.37.110.26');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (8, 'Scarlet', 'Grafham', 'sgrafham7@usatoday.com', 'Female', '111.142.177.83');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (9, 'Reed', 'Cradduck', 'rcradduck8@yandex.ru', 'Male', '123.231.68.223');
+insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (10, 'Fiorenze', 'McCobb', 'fmccobb9@example.com', 'Female', '252.37.53.63');
